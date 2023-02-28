86796970e440ded6a3879283e0ed381b0cfea7cd
lcz <383723617@qq.com>
2023-02-28 12:53:34 +0800

    相关注释补充

--- src/network/request.js
+++ src/network/request.js
@@ -3,13 +3,13 @@ import axios from "axios";
 export function request(config) {
     // 1.创建axios的实例
     const instance = axios.create({
-      baseURL: 'http://152.136.185.210:7878/api/hy66',
+      baseURL: 'http://152.136.185.210:7878/api/hy66',     //后端服务器的地址
       timeout: 5000
     })
 
 
 // 2.axios的拦截器
-  // 2.1.请求拦截的作用
+  // 2.1.请求拦截的作用   对请求进行拦截响应
   instance.interceptors.request.use(config => {
     return config
   }, err => {
